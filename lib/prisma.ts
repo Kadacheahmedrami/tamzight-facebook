6ce97ca21f248eff159b8d0d296525636ea8e536
Kadacheahmedrami <a_kadache@estin.dz>
2025-07-03 02:09:52 +0200

    initiate a prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // Prevent creating multiple instances in development
+  // (especially when using hot‑reload / Fast Refresh)
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
